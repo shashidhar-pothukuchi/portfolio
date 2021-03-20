5fd9c7b5d363131afee2bbc66cd810733f6b010c
ammmg <47329981+ammmg@users.noreply.github.com>
2021-03-20 15:47:54 +0530

    Delete calculateNetworth/calculateNetworth directory

--- calculateNetworth/calculateNetworth/src/main/resources/schema.sql
+++
@@ -1,3 +0,0 @@
-create table portfolio (portfolio_id int primary key);
-
-create table asset (tid int primary key,assetid varchar(20),portfolioid int,type varchar(10),units int, foreign key(portfolioid) references portfolio(portfolio_id));
