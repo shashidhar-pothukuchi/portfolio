c702e735135f14cabe5c6fa2962929c8da462937
ammmg <47329981+ammmg@users.noreply.github.com>
2021-03-20 13:14:06 +0530

    Add files via upload

---
+++ Daily-Share-Price/Daily-Share-Price/src/test/java/com/cognizant/portfolio_management/DailySharePrice/DailySharePriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.portfolio_management.DailySharePrice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailySharePriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
