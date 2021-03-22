5763c1448607fe20dbb7e84bce3a56a8fe529e42
Shashidhar <saithechinnu@gmail.com>
2021-03-22 10:40:50 +0530

    Delete calculateNetworth/calculateNetworth directory

--- calculateNetworth/calculateNetworth/src/main/java/com/cognizant/calculateNetworth/reposotory/PortfolioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cognizant.calculateNetworth.reposotory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cognizant.calculateNetworth.model.Portfolio;
-
-public interface PortfolioRepository extends JpaRepository<Portfolio,Integer>{
-
-}
