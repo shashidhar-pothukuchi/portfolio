1a188edf3d2457eb226cb3f9ef1cd6dcd94daa59
Shashidhar <saithechinnu@gmail.com>
2021-03-22 11:00:31 +0530

    Added jwt authorization

---
+++ WebController/WebController/src/test/java/com/cognizant/pharmacymanagement/WebPortal/WebPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.pharmacymanagement.WebPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
